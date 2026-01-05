f61e2e3b4857d9862152db23778f626acd33c02f
Abhishek <siyaabhi441@gmail.com>
2026-01-06 02:43:52 +0530

    4A

---
+++ prob.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class prob{
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        if(n%2 ==0 && n > 2){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+        
+    }
+}
